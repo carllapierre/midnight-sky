151ccde9220f1274094f47f9c5a4a0e214ae602a
carllapierre1 <carl_lapi@hotmail.com>
2020-09-21 20:31:42 -0400

    a

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import MainContainer from './Components/Container/MainContainer';
 class App extends Component {
   render() {
     return <Router>
-      <Route path='/web/slideshow/view/1FXiKRtT3sgAn_LBbWpzxKg5ezIoW4Edz0m-lqvhh7PI' component={() => { 
+      <Route path='/' component={() => { 
           window.location.href = 'https://www.youtube.com/watch?v=oHg5SJYRHA0&ab_channel=cotter548'; 
           return null;
      }}/>
